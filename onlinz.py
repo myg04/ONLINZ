86b4d1f3a9ebb498dcd6907b8d0209c229cae977
myg <no-reply>
2021-10-05 09:30:01 +1300

    Version 2

--- onlinz.py
+++ onlinz.py
@@ -1,8 +1,9 @@
 def box_dimensions():
     while True:
         try:
-            height = int(input("Hi {}, What is the height of your return box in cm? " .format(name)))
-            if height > 5 and height < 100:
+            global user_height
+            user_height = int(input("Hi {}, What is the height of your return box in cm? " .format(name)))
+            if user_height > 5 and user_height < 100:
                 break
             else:
                 print("Please enter a height between 5 and 100 cm")
@@ -11,8 +12,9 @@ def box_dimensions():
 
     while True:
         try:
-            width = int(input("Hi {}, What is the width of your return box in cm? " .format(name)))
-            if width > 5 and width < 100:
+            global user_width
+            user_width = int(input("Hi {}, What is the width of your return box in cm? " .format(name)))
+            if user_width > 5 and user_width < 100:
                 break
             else:
                 print("Please enter a width between 5 and 100 cm")
@@ -21,19 +23,26 @@ def box_dimensions():
 
     while True:
             try:
-                depth = int(input("Hi {}, What is the depth of your return box in cm? " .format(name)))
-                if depth > 5 and depth < 100:
+                global user_depth
+                user_depth = int(input("Hi {}, What is the depth of your return box in cm? " .format(name)))
+                if user_depth > 5 and user_depth < 100:
                     break
                 else:
                     print("Please enter a depth between 5 and 100 cm")
             except ValueError:
                 print("Please enter a valid number")
-    print("Your box dimensions are: \nHeight = {}cm \nWidth = {}cm \nDepth = {}cm" .format(height, width, depth))
+    print("Your box dimensions are: \nHeight = {}cm \nWidth = {}cm \nDepth = {}cm" .format(user_height, user_width, user_depth))
+    
+    return user_height, user_width, user_depth
+
+def calc_volume(height, width, depth):
+    global volume
+    volume = height * width * depth
+    print("Your box volume is {} cm\u00b3" .format(volume))
+    return volume
 
 
-       
-
-
-
+#main program
 name = input("Hello customer, what is your name?").title()
 box_dimensions()
+calc_volume(user_height, user_width, user_depth)
